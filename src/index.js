65d741ceb2ca6cd06e6cc3022c684a4427715c10
Abinash Mohapatra <abinash@expensebit.com>
2020-12-23 13:33:20 +0530

    Implement Speechly

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SpeechProvider } from '@speechly/react-client';
 
 import './index.css';
 
@@ -7,8 +8,10 @@ import App from './App';
 import { Provider } from './context/context';
 
 ReactDOM.render(
-    <Provider>
-        <App />
-    </Provider>,
+    <SpeechProvider appId="58a5babe-d92e-4b0d-915a-ad67675ae3ce" language="en-US">
+        <Provider>
+            <App />
+        </Provider>
+    </SpeechProvider>,
     document.getElementById('root')
 );
