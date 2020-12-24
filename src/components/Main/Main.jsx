fb3f5bade0f4d349243226630bda904ca1eb8e4e
Abinash Mohapatra <abinash@expensebit.com>
2020-12-24 14:16:44 +0530

    Add localStorage

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+import { ExpenseTrackerContext } from '../../context/context';
 
 import useStyles from './styles';
 import Form from './Form/Form';
@@ -8,12 +9,13 @@ import List from './List/List';
 const Main = () => {
 
     const classes = useStyles();
+    const { balance } = useContext(ExpenseTrackerContext);
 
     return (
         <Card className={classes.root}>
             <CardHeader title="Expense Tracker" />
             <CardContent>
-                <Typography align="center" variant="h5">Total Balance $100</Typography>
+                <Typography align="center" variant="h5">Total Balance ${balance}</Typography>
                 <Typography variant="subtitle1" style={{ lineHeight: "1.5em", marginTop: '28px' }}>
                     Try Saying: Add income for $100 in catagory salary from Monday...
                 </Typography>
